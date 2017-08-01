397199cf831b276377e6d4689913962a7f3d1f39
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-01 07:59:40 -0600

    add search funcionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,23 @@
 class ItemsController < ApplicationController
+
   def index
-    @items = Item.all
+    #@items = Item.search(params[:search_term])
+    @items = if params[:search_term]
+    # LIKE pattern matching expression returns true if the string matches
+      Item.where('name LIKE ?', "%#{params[:search_term]}%")
+      #Item.where('description LIKE ?', "%#{params[:search_term]}%")
+    else
+      Item.all
+    end
   end
 
   def show
     @item = Item.find(params[:id].to_i)
   end
+
+  private
+
+  def item_params
+  params.require(:item).permit(:name, :description, :image_url, :search_term)
+end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ActiveRecord::Base
   has_many :order_items
   has_many :orders, through: :order_items
+
+  # def self.search(search_term)
+  #   if search_term
+  #     where('name LIKE ?', "%#{search_term}%")
+  #     #where('name LIKE ?', "%#{search_term}%").limit(10)
+  #     #where('name LIKE ?', "%#{search_term}%").order('id DESC')
+  #   else
+  #     order('id DESC')
+  #   end
+  # end
 end
